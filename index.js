d3746bb7fee4f9b3c0287a288d94292888475b6a
Andres Parra <andresdavidparra2002@gmail.com>
2024-05-16 12:31:07 -0500

    models import

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const bodyParser = require("body-parser");
+const { User, Product, Purchase } = require("./model/models");
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.json());
+let users = [];
+let products = [];
+let purchases = [];
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
